1e422c269a09f87374a741c5dc1adeffbf3b0403
Leila Shekofteh <leila.shekofteh@gmail.com>
2024-05-11 14:37:00 +0200

    refactor

--- src/main/java/task/engineering/Main.java
+++ src/main/java/task/engineering/Main.java
@@ -4,23 +4,6 @@ import java.util.*;
 
 public class Main {
 
-    // Algorithm 1
-    // with time complexity of O((n-k)k)
-    // best case k = 1 or k = n --> O(n)
-    // worst case k = n/2 --> O(n^2)
-
-
-
-
-    // Algorithm 2
-    // best case is when we can partition the array in halves --> O(n)
-    // worst case is when we choose the largest/smallest value as the pivot --> O(n^2)
-
-
-
-
-
-    // Execution
     public static void main(String[] args) {
 
         int k = 2;
